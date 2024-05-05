2b7ab8575c1ea1a6a134222e5f5e671edd9a63c3
H.Yamada <ymd_h@outlook.jp>
2024-05-05 18:34:06 +0900

    Update header comment

--- yapilot.el
+++ yapilot.el
@@ -10,8 +10,25 @@
 ;; This file is not part of GNU Emacs.
 
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
-;; This package provide copilot-like functions based on LLM.
+;; This package provides copilot-like functionalities using LLM.
+;; This package calls LLM through ahyatt/llm <https://github.com/ahyatt/llm>,
+;; so that you need to set one of the llm providers to `yapilot-llm-provider'.
+
 
 ;;; Code:
 (require 'llm)
